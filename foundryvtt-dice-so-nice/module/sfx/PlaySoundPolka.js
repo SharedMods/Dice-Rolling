80bc8206040d5d5cf7009f77927263179cdc416f
SharedMods <83603605+SharedMods@users.noreply.github.com>
2021-05-04 16:30:41 -0500

    Update PlaySoundPolka.js

--- foundryvtt-dice-so-nice/module/sfx/PlaySoundPolka.js
+++ foundryvtt-dice-so-nice/module/sfx/PlaySoundPolka.js
@@ -3,7 +3,7 @@ import { DiceSFX } from '../DiceSFX.js';
 export class PlaySoundPolka extends DiceSFX {
     static id = "PlaySoundPolka";
     static name = "DICESONICE.PlaySoundPolka";
-    static path = 'modules/dice-so-nice/sfx/sounds/Polka_knows_nat_one.mp3';
+    static path = 'modules/dice-so-nice/sfx/sounds/Polka_knows_nat_one.wav';
     /**@override init */
     static async init(){
         game.audio.pending.push(function(){

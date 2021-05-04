26e879bc9867ca2f29b6ed5d073f71095ef9e830
SharedMods <83603605+SharedMods@users.noreply.github.com>
2021-05-04 16:23:51 -0500

    Update DiceSFXManager.js

--- foundryvtt-dice-so-nice/module/DiceSFXManager.js
+++ foundryvtt-dice-so-nice/module/DiceSFXManager.js
@@ -1,6 +1,7 @@
 import { PlaySoundEpicFail } from './sfx/PlaySoundEpicFail.js';
 import { PlaySoundEpicWin } from './sfx/PlaySoundEpicWin.js';
 import { PlaySoundPeko } from './sfx/PlaySoundPeko.js';
+import { PlaySoundGoodMorning } from './sfx/PlaySoundGoodMorning.js';
 import { PlayConfettiStrength1 } from './sfx/PlayConfettiStrength1.js';
 import { PlayConfettiStrength2 } from './sfx/PlayConfettiStrength2.js';
 import { PlayConfettiStrength3 } from './sfx/PlayConfettiStrength3.js';
